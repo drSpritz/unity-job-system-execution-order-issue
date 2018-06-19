a474e69f2a312e92b4ac23f6c5dada12e705bf45
drspriz <drspritz@gmail.com>
2018-06-19 10:57:34 +0300

    initial

---
+++ Assets/Game/Code/Spawner.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Rendering;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+	public int instancesCount;
+    public Mesh mesh;
+    public Material mat;
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        EntityManager manager = World.Active.GetOrCreateManager<EntityManager>();
+
+        EntityArchetype archetype = manager.CreateArchetype(
+            ComponentType.Create<MeshInstanceRenderer>(),
+            ComponentType.Create<TransformMatrix>(),
+            ComponentType.Create<Position>(),
+            ComponentType.Create<Rotation>(),
+            ComponentType.Create<MoveForward>(),
+            ComponentType.Create<MoveSpeed>(),
+            ComponentType.Create<Speed>(),
+            ComponentType.Create<ModifySpeed>()
+            );
+
+        Entity entity = manager.CreateEntity(archetype);
+
+        manager.SetSharedComponentData(entity, new MeshInstanceRenderer() { mesh = mesh, material = mat });
+
+        var instances = new NativeArray<Entity>(instancesCount, Allocator.Temp);
+        manager.Instantiate(entity, instances);
+        manager.DestroyEntity(entity);
+
+        Vector3 initialPosition = transform.position;
+
+        foreach (Entity e in instances)
+        {
+            manager.SetComponentData(e, new Position() { Value = initialPosition + Random.onUnitSphere * Random.Range(0f, 100f) });
+            manager.SetComponentData(e, new Rotation() { Value = Random.rotation });
+            manager.SetComponentData(e, new Speed() { Value = 100f });
+            manager.SetComponentData(e, new ModifySpeed() { Value = 0f });
+        }
+
+        instances.Dispose();
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+Logic of working a systems is very simple. The project have two systems, first one "SpeedSystem" - copy value (100f) from "Speed" component to "MoveSpeed" component. Second system "ModifySpeedSystem" just multiply "MoveSpeed" component value (100f) by self value (0f). The order of execution of these systems is strictly specified - "SpeedSystem" calls before "MoveForwardSystem" and "ModifySpeedSystem" calls before "MoveForwardSystem" but after "SpeedSystem".
+
+If you open SampleScene from the project, you can find two game objects, called "IgnoreSystemsOrder_Spawner" and "SuccessSystemsOrder_Spawner". Difference between theese objects is just an instances count, that script will be spawn.
+
+First case:
+- Enable "SuccessSystemsOrder_Spawner" and hit play. As result you will be see cloud of 1000 cubes. Theese cubes won't move, because "ModifySpeedSystem" multiply 100f by 0f and get 0f as result.
+
+Second case:
+- Enable "IgnoreSystemsOrder_Spawner" and hit play. As result you will be see cloud of 100000 cubes. Theese cubes will be move!
+
+As i understand, when CPU get high loads the systems execution order or Jobs dependency not respects.
